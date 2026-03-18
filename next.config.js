f3040b72f3990f0011932cacaba400fd53021fa2
Akubrecah <poweldayck@gmail.com>
2026-03-18 23:37:38 +0300

    Revert basePath for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -11,10 +11,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 const nextConfig = {
   // Enable static export for deployment flexibility
   output: 'export',
-  
-  // Set basePath and assetPrefix for GitHub Pages repo-based URL
-  basePath: '/Akubrecah-Dev-Cave-PDF',
-  assetPrefix: '/Akubrecah-Dev-Cave-PDF/',
 
   // Webpack configuration for WASM modules
   webpack: (config, { isServer, webpack }) => {
